cf5fa1eab8ba85754ab3e621623b9990459a94c5
Khaleel Smith <145707575+ksmithereens@users.noreply.github.com>
2023-11-28 19:38:55 +0000

    Scroll Triggers

--- README.md
+++ README.md
@@ -2,6 +2,11 @@
 
 > Colin likes coffee.
 
-Stuff about coffee. Coffee is
+Stuff about coffee. Coffee is  
 
 DIY Template, starter, boilerplate, prototype, etc. for future Web Design projects. 
+
+## Citations
+* GitHub CoPilot
+[Coffee] (https://www.wikipedia.org/wiki/Coffee)
+[Favicon.io] (https://favicon.io/)
--- script.js
+++ script.js
@@ -1,1 +1,55 @@
 console.log( '😀👍🍎' );
+// GSAP library
+// https://unpkg.co/gsap@3/dist/gsap.min.js
+
+// ScrollTrigger plugin
+// https://unpkg.com/gsap@3/dist/ScrollTrigger.min.js
+
+// Install & Active Plugin
+gsap.registerPlugin(ScrollTrigger);
+
+// defaults
+ScrollTrigger.defaults({
+  toggleActions: "restart pause resume pause"
+});
+
+// Panel One
+gsap.to(".one h1", {
+  scrollTrigger: "one",
+  color: "brown",
+  duration: 3, 
+  rotate: 0, 
+  opacity: 1
+});
+
+// Panel Two
+gsap.to(".two", {
+  scrollTrigger: "two"
+});
+
+// Panel Three
+gsap.to(".three p", {
+  
+  scrollTrigger: "three",
+  letterSpacing: 0, 
+  duration: 5
+
+});
+
+// Panel Four
+gsap.to(".four", {
+  scrollTrigger: "four"
+});
+
+// Panel Five
+gsap.to(".five", {
+  scrollTrigger: "five"
+});
+
+
+// Panel Six
+gsap.to(".six", {
+  scrollTrigger: "six",
+  translateX : 0, 
+  duration: 5
+});
